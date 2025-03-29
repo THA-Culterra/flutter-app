463033b86a9aa3703c9b6d9cf23133f636ce632a
Samy Abderraouf Mehdid <sr.mehdid@gmail.com>
2025-03-29 03:10:18 +0100

    added login UI

---
+++ lib/screens/Login/domain/provider.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+enum Provider {
+  google(
+    title: "Google",
+    icon: "google",
+    colors: [Color(0xFFF65339), Color(0xFFF68262)],
+  ),
+  facebook(
+    title: "Facebook",
+    icon: "facebook",
+    colors: [Color(0xFF1D24FF), Color(0xFF4744FB)],
+  ),
+  apple(
+    title: "Apple",
+    icon: "apple",
+    colors: [Color(0xFF000000), Color(0xFF333333)],
+  ),
+  email(
+    title: "Email",
+    icon: "email",
+    colors: [Color(0xFF8539F6), Color(0xFF9E62F6)],
+  );
+
+  final String title;
+  final String icon;
+  final List<Color> colors;
+
+  const Provider({
+    required this.title,
+    required this.icon,
+    required this.colors,
+  });
+}
