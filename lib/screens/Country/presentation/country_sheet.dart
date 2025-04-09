fb95a53cb96bb6f2380e2cc65a52f4e8b6e8b01a
Samy Abderraouf Mehdid <sr.mehdid@gmail.com>
2025-04-09 11:04:02 +0200

    added interactive sticky scroll

--- lib/screens/Country/presentation/country_sheet.dart
+++ lib/screens/Country/presentation/country_sheet.dart
@@ -27,67 +27,101 @@ class _CountrySheetState extends State<CountrySheet> {
                   color: Colors.white,
                   borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
                 ),
-                child: ListView(
+                child: CustomScrollView(
                   controller: scrollController,
-                  children: [
-                    // Horizontal Scrolling Category List
-                    SizedBox(
-                      height: 68,
-                      child: ListView(
-                        scrollDirection: Axis.horizontal,
-                        shrinkWrap: true,
-                        children:
-                            CountryCategory.values.map((category) {
-                              return GestureDetector(
-                                onTap: () {
-                                  setState(() {
-                                    selectedCategory = category;
-                                  });
-                                },
-                                child: Padding(
-                                  padding: EdgeInsets.symmetric(horizontal: 10),
-                                  child: Column(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      SvgPicture.asset(
-                                        "lib/core/assets/${category.icon}.svg", // Path to your SVG asset
-                                        colorFilter: ColorFilter.mode(
-                                          selectedCategory == category ? Colors.purple : Colors.grey,
-                                          BlendMode.srcIn,
-                                        ),
-                                        width: 24,
-                                        height: 24,
-                                      ),
-                                      Text(
-                                        category.displayName,
-                                        style: TextStyle(
-                                          decoration: TextDecoration.none,
-                                          fontSize: 16,
-                                          fontWeight:
-                                              selectedCategory == category
-                                                  ? FontWeight.bold
-                                                  : FontWeight.normal,
-                                          color:
-                                              selectedCategory == category
-                                                  ? Colors.purple
-                                                  : Colors.black,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              );
-                            }).toList(),
+                  slivers: [
+                    // Sticky category row
+                    SliverPersistentHeader(
+                      pinned: true, // Makes it stick to the top
+                      floating: false,
+                      delegate: CategoryHeaderDelegate(
+                        selectedCategory: selectedCategory,
+                        onCategorySelected: (category) {
+                          setState(() {
+                            selectedCategory = category;
+                          });
+                        },
                       ),
                     ),
-                Container(
-                  color: Colors.grey[100],
-                  child: Flexible(child: selectedCategory.content)
-                )],
-              ),
+
+                    // Scrolling content
+                    SliverList(
+                      delegate: SliverChildListDelegate([
+                        Container(
+                          color: Colors.grey[100],
+                          child: selectedCategory.content,
+                        ),
+                      ]),
+                    ),
+                  ],
+                ),
+
           );
         },
       ),
     );
   }
+}
+
+class CategoryHeaderDelegate extends SliverPersistentHeaderDelegate {
+  final CountryCategory selectedCategory;
+  final Function(CountryCategory) onCategorySelected;
+
+  CategoryHeaderDelegate({required this.selectedCategory, required this.onCategorySelected});
+
+  @override
+  double get minExtent => 68; // Minimum height when scrolled to the top
+  @override
+  double get maxExtent => 68; // Fixed height
+
+  @override
+  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
+    double radius = shrinkOffset > 0 ? 0 : 16;
+
+    return Material( // Wrap with Material to ensure decoration applies correctly
+      elevation: shrinkOffset > 0 ? 4 : 0, // Optional shadow for smooth effect
+      color: Colors.white, // Ensure background color applies correctly
+      borderRadius: BorderRadius.vertical(top: Radius.circular(radius)),
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 8),
+        child: ListView(
+          scrollDirection: Axis.horizontal,
+          shrinkWrap: true,
+          children: CountryCategory.values.map((category) {
+            return GestureDetector(
+              onTap: () => onCategorySelected(category),
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SvgPicture.asset(
+                      "lib/core/assets/${category.icon}.svg",
+                      colorFilter: ColorFilter.mode(
+                        selectedCategory == category ? Colors.purple : Colors.grey,
+                        BlendMode.srcIn,
+                      ),
+                      width: 24,
+                      height: 24,
+                    ),
+                    Text(
+                      category.displayName,
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: selectedCategory == category ? FontWeight.bold : FontWeight.normal,
+                        color: selectedCategory == category ? Colors.purple : Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          }).toList(),
+        ),
+      ),
+    );
+  }
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) => true;
 }
