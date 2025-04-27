76d402fe92f9f837ad82c226280b24483cb22f77
Samy Abderraouf Mehdid <sr.mehdid@gmail.com>
2025-04-27 16:41:43 +0200

    fixed history text fonts

--- lib/screens/Country/presentation/features/history.dart
+++ lib/screens/Country/presentation/features/history.dart
@@ -9,33 +9,50 @@ class HistoryScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text("National Day"),
-        Text(history.nationalDay.toString())
-      ],
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          holidaysBuilder(history.nationalDay.entries.first)
+        ],
+      ),
     );
   }
 
-  Widget holidaysBuilder(History history) {
-    final nationalDayEntry = history.nationalDay.entries.first;
+  Widget holidaysBuilder(MapEntry<String, DateTime> nationalDayEntry) {
 
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text(
           "National Day",
-          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          style: TextStyle(
+              fontSize: 14,
+              color: Colors.black38,
+              fontWeight: FontWeight.bold,
+              decoration: TextDecoration.none
+          ),
         ),
         SizedBox(height: 8),
         Text(
           nationalDayEntry.key, // Name of the National Day (e.g., Independence Day)
-          style: TextStyle(fontSize: 18),
+          style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+              decoration: TextDecoration.none
+          ),
         ),
         SizedBox(height: 4),
         Text(
           "${nationalDayEntry.value.day}/${nationalDayEntry.value.month}/${nationalDayEntry.value.year}",
-          style: TextStyle(fontSize: 16, color: Colors.grey),
+          style: TextStyle(
+              fontSize: 16,
+              color: Colors.grey,
+              fontWeight: FontWeight.normal,
+              decoration: TextDecoration.none
+          ),
         ),
       ],
     );
